bfdcbb9ec8a49d61fe0825766dbc224b6e902168
haydenroberts130 <102333212+haydenroberts130@users.noreply.github.com>
2024-04-13 16:20:41 -0700

    updated svg

--- src/view_controller/GameBoard.java
+++ src/view_controller/GameBoard.java
@@ -309,7 +309,8 @@ public class GameBoard extends Application {
 	public void refreshStyle() {
 		if (darkModeToggle.isDarkMode()) {
 			wholeGameStackPane.setStyle(styleManager.getWholeGameDarkMode());
-			achievementsIconSVG.setFill(styleManager.colorText());
+			achievementsIconSVG.setStroke(styleManager.colorText());
+			achievementsIconSVG.setFill(Color.BLACK);
 			shuffleButton.setStyle(styleManager.getButtonDarkMode());
 			deselectButton.setStyle(styleManager.getButtonDarkMode());
 			if (this.getSelectedCount() == GameBoard.MAX_SELECTED && !submitButton.isDisabled()) {
@@ -328,7 +329,8 @@ public class GameBoard extends Application {
 
 		} else {
 			wholeGameStackPane.setStyle(styleManager.getWholeGameNormalMode());
-			achievementsIconSVG.setFill(styleManager.colorText());
+			achievementsIconSVG.setStroke(styleManager.colorText());
+			achievementsIconSVG.setFill(Color.WHITE);
 			shuffleButton.setStyle(styleManager.getButtonNormalMode());
 			deselectButton.setStyle(styleManager.getButtonNormalMode());
 			if (this.getSelectedCount() == GameBoard.MAX_SELECTED && !submitButton.isDisabled()) {
@@ -529,10 +531,14 @@ public class GameBoard extends Application {
 		// Create an HBox to hold the dark mode toggle, achievements, and leader board
 		achievementsIconSVG = new SVGPath();
 		achievementsIconSVG.setContent(
-				"M60,4H48c0-2.215-1.789-4-4-4H20c-2.211,0-4,1.785-4,4H4C1.789,4,0,5.785,0,8v8c0,8.836,7.164,16,16,16  c0.188,0,0.363-0.051,0.547-0.059C17.984,37.57,22.379,41.973,28,43.43V56h-8c-2.211,0-4,1.785-4,4v4h32v-4c0-2.215-1.789-4-4-4h-8  V43.43c5.621-1.457,10.016-5.859,11.453-11.488C47.637,31.949,47.812,32,48,32c8.836,0,16-7.164,16-16V8C64,5.785,62.211,4,60,4z   M8,16v-4h8v12C11.582,24,8,20.414,8,16z M56,16c0,4.414-3.582,8-8,8V12h8V16z");
-
-		achievementsIconSVG.setScaleX(0.5203125);
-		achievementsIconSVG.setScaleY(0.5203125);
+				"M12 14V17M12 14C9.58104 14 7.56329 12.2822 7.10002 10M12 14C14.419 14 16.4367 12.2822 16.9 10M17 5H19.75C19.9823 5 20.0985 5 20.1951 5.01921C20.5918 5.09812 20.9019 5.40822 20.9808 5.80491C21 5.90151 21 6.01767 21 6.25C21 6.94698 21 7.29547 20.9424 7.58527C20.7056 8.77534 19.7753 9.70564 18.5853 9.94236C18.2955 10 17.947 10 17.25 10H17H16.9M7 5H4.25C4.01767 5 3.90151 5 3.80491 5.01921C3.40822 5.09812 3.09812 5.40822 3.01921 5.80491C3 5.90151 3 6.01767 3 6.25C3 6.94698 3 7.29547 3.05764 7.58527C3.29436 8.77534 4.22466 9.70564 5.41473 9.94236C5.70453 10 6.05302 10 6.75 10H7H7.10002M12 17C12.93 17 13.395 17 13.7765 17.1022C14.8117 17.3796 15.6204 18.1883 15.8978 19.2235C16 19.605 16 20.07 16 21H8C8 20.07 8 19.605 8.10222 19.2235C8.37962 18.1883 9.18827 17.3796 10.2235 17.1022C10.605 17 11.07 17 12 17ZM7.10002 10C7.03443 9.67689 7 9.34247 7 9V4.57143C7 4.03831 7 3.77176 7.09903 3.56612C7.19732 3.36201 7.36201 3.19732 7.56612 3.09903C7.77176 3 8.03831 3 8.57143 3H15.4286C15.9617 3 16.2282 3 16.4339 3.09903C16.638 3.19732 16.8027 3.36201 16.901 3.56612C17 3.77176 17 4.03831 17 4.57143V9C17 9.34247 16.9656 9.67689 16.9 10");
+		achievementsIconSVG.setStrokeLineCap(javafx.scene.shape.StrokeLineCap.ROUND);
+		achievementsIconSVG.setStrokeLineJoin(javafx.scene.shape.StrokeLineJoin.ROUND);
+		achievementsIconSVG.setStrokeWidth(2);
+		achievementsIconSVG.setStroke(Color.BLACK);
+		achievementsIconSVG.setFill(Color.WHITE);
+		achievementsIconSVG.setScaleX(1.58571428571);
+		achievementsIconSVG.setScaleY(1.58571428571);
 
 		HBox cornerButtonBox = new HBox(10, achievementsIconSVG, darkModeToggle);
 		cornerButtonBox.setStyle("-fx-alignment: center-right;");
